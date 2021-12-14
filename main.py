c960e917763f639d8658bf6d9c5f8ce40beabb82
potassiummmm <1574996073@qq.com>
2021-12-14 12:44:18 +0800

    Update results on sock-shop dataset with alpha=0.05, configuration setting script and sample main function

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
+from runner.cloud_ranger_runner import CloudRangerRunner
+import datetime
+import os
 import json
+import gc
 
+
+# Update config
+def update_config(start_index, batch_size):
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    path = base_dir + '/data/sock-shop/metric/'
+    file_list = []
+    for file in os.listdir(path):
+        if file.endswith('.csv'):
+            file_list.append(file[:-4])
+    file_list.sort()
+    with open(base_dir + '/data/sock-shop/label.json', 'w+') as f:
+        data = {}
+        for i in range(start_index, min(start_index + batch_size, len(file_list))):
+            data[file_list[i]] = "0"
+        json.dump(data, f)
+    with open(base_dir + '/config/data_rca_model_name_sock-shop.json', 'w+') as f:
+        data = {"train": [], "valid": [], "test": [], "data_type": {"metric": True, "tracing": False, "logging": False}}
+        lst = [file_list[i] for i in range(start_index, min(start_index + batch_size, len(file_list)))]
+        data["test"] = lst
+        data["train"] = lst
+        json.dump(data, f)
+
+
+# Run
 if __name__ == '__main__':
-    import os
-    base_path = 'saved/model/cloud_ranger_runner/sock_shop/alpha_0_15/score_ranking_list/'
-    for file in os.listdir(base_path):
-        if file.endswith("json"):
-            with open((os.path.abspath(base_path + file))) as f:
-                for k, v in json.load(f).items():
-                    if len(v) == 0:
-                        print(k)
+    for i in [0, 60, 120, 180, 240]:
+        update_config(i, 60)
+        print('start:', datetime.datetime.now())
+        cloud_ranger_runner = CloudRangerRunner()
+        cloud_ranger_runner.run()
+        cloud_ranger_runner.test()
+        print('finished:', datetime.datetime.now())
+        del cloud_ranger_runner
+        gc.collect()
+
+
